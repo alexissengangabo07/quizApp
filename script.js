387a20830162c8f74381054df849e45c3a449083
alexsenga <alexxngabo@gmail.com>
2022-06-18 17:09:20 +0200

    feat & style: style cards elements

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ let secondeCounter = setInterval(() => {
     }
     i--;
 }, 600);
+
+let radioInputs = document.querySelectorAll(`input[type="radio"]`);
+
+radioInputs.forEach((radio) => {
+    radio.addEventListener('change', function() {
+        console.log(radioInputs);
+        if (radio.checked) {
+            radioInputs.forEach(r => r.parentElement.parentElement.style.borderColor = 'rgb(233, 231, 231)')
+            this.parentElement.parentElement.style.borderColor = 'green'
+        } else {
+            this.parentElement.parentElement.style.borderColor = 'rgb(233, 231, 231)';
+        }
+    });
+})
