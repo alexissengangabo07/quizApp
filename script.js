36bb4becd5d620dd3b6af9ec56cf19a7e902db95
alexsenga <alexxngabo@gmail.com>
2022-06-18 14:05:21 +0200

    style: card questions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let i = 100;
 let secondeCounter = setInterval(() => {
     if (i > 0) {
         document.querySelector('.progress-bar').style.width = i + '%';
-        seconde.innerHTML = Math.floor(i / 1.666666667);
+        seconde.innerHTML = Math.ceil(i / 1.666666667);
         if (i < 30) {
             document.querySelector('.progress-bar').style.background = 'red';
         }
